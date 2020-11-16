5fc75ef3e92e47ddc83b56358126dc329e3d5e46
Erich Healy <ehealy@greenbits.com>
2020-11-16 07:51:57 -0800

    Start on file parser

--- src/block/magic.rs
+++
@@ -1,6 +0,0 @@
-// Parsing information for the Marker block in a rar archive
-
-pub enum RarVersion {
-    Rar3,
-    Rar5,
-}
--- src/block/mod.rs
+++ src/block/mod.rs
@@ -11,29 +11,25 @@ use crc::{crc32, Hasher32};
 
 mod common;
 
-// pub enum Block {
-//     Marker(MarkerBlock),
-//     Archive,
-//     Unsupported,
-// }
+// HEAD_TYPE=0x72          marker block
+// HEAD_TYPE=0x73          archive header
+// HEAD_TYPE=0x74          file header
+// HEAD_TYPE=0x75          old style comment header
+// HEAD_TYPE=0x76          old style authenticity information
+// HEAD_TYPE=0x77          old style subblock
+// HEAD_TYPE=0x78          old style recovery record
+// HEAD_TYPE=0x79          old style authenticity information
+// HEAD_TYPE=0x7a          subblock
 
-// pub struct MarkerBlock(BlockHeader);
-
-// impl MarkerBlock {
-//     fn header_crc_verified(&self) -> bool {
-//         unimplemented!()
-//     }
-// }
-
-// RAR3 magic = [82, 97, 114, 33, 26, 7, 0]
-// RAR5 magic = [82, 97, 114, 33, 26, 7, 1, 0]
+const RAR3_MAGIC: [u8; 7] = [82, 97, 114, 33, 26, 7, 0];
+const RAR5_MAGIC: [u8; 8] = [82, 97, 114, 33, 26, 7, 1, 0];
 
 pub fn rar3_marker_block(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    tag(&[82, 97, 114, 33, 26, 7, 0])(input)
+    tag(&RAR3_MAGIC)(input)
 }
 
 pub fn rar5_marker_block(input: &[u8]) -> IResult<&[u8], &[u8]> {
-    tag(&[82, 97, 114, 33, 26, 7, 1, 0])(input)
+    tag(&RAR5_MAGIC)(input)
 }
 
 pub struct ArchiveFlags(u16);
@@ -106,6 +102,44 @@ impl Debug for ArchiveFlags {
     }
 }
 
+struct BlockPreamble {
+    declared_header_crc: u16,
+    flags: u16,
+    header_size: u64,
+    rolling_crc: crc32::Digest,
+}
+
+fn block_preamble(
+    required_marker: u8,
+) -> impl Fn(&[u8]) -> IResult<&[u8], &[u8], nom::error::ParseError> {
+    move |input: &[u8]| {
+        // let declared_crc = le_u16;
+        // let marker = tag(&[required_marker]);
+        // let flags = le_u16;
+        // let head_size = le_u16;
+
+        let (start_rest, (declared_crc, _, flags, low_head_size)) =
+            tuple((le_u16, tag(&[required_marker]), le_u16, le_u16))(input)?;
+
+        let (rest, header_size) = if flags & 0x8000 != 0 {
+            let (end_rest, large_head_size) = le_u32(start_rest)?;
+            (end_rest, low_head_size as u32 + large_head_size)
+        } else {
+            (start_rest, low_head_size as u32)
+        };
+
+        Ok((
+            rest,
+            BlockPreamble {
+                declared_header_crc: declared_crc,
+                flags: flags,
+                header_size: header_size,
+                rolling_crc: crc32::Digest::new(0xEDB88320),
+            },
+        ))
+    }
+}
+
 #[derive(Debug)]
 pub struct ArchiveHeader {
     head_crc: u16,
@@ -143,4 +177,77 @@ pub fn archive_header(input: &[u8]) -> IResult<&[u8], ArchiveHeader> {
     };
 
     Ok((rest, ah))
+}
+
+#[derive(Debug)]
+pub struct FileFlags(u16);
+
+impl FileFlags {
+    fn is_continued_from_previous_volume(&self) -> bool {
+        self.0 & 0x01 != 0
+    }
+    fn is_continued_in_next_volume(&self) -> bool {
+        self.0 & 0x02 != 0
+    }
+    fn requires_password(&self) -> bool {
+        self.0 & 0x04 != 0
+    }
+    fn file_comment_present(&self) -> bool {
+        // rar version 3 and later use the separate comment block instead of
+        // inline comments.
+        self.0 & 0x08 != 0
+    }
+    fn uses_high_size_fields(&self) -> bool {
+        // Look for high_pack_size and high_unpack_size if this is set
+        self.0 & 0x100 != 0
+    }
+    fn has_unicode_filename(&self) -> bool {
+        // Should trigger special filename parsing to look for the unicode
+        // filename
+        self.0 & 0x200 != 0
+    }
+    fn has_salt(&self) -> bool {
+        self.0 & 0x400 != 0
+    }
+    fn file_is_versioned(&self) -> bool {
+        self.0 & 0x800 != 0
+    }
+    fn has_ext_time(&self) -> bool {
+        self.0 & 0x1000 != 0
+    }
+}
+
+#[derive(Debug)]
+pub struct FileHeader {
+    head_crc: u16,
+    head_type: u8,
+    head_flags: FileFlags,
+    // head_size: u16,
+    // pack_size: u32,
+    // unpack_size: u32,
+    // host_os: u8, // enumify this
+    // file_crc: u32,
+    // ftime: u32,
+    // unpack_version: u8, // Rar version needed to unpack this.
+    // packing_method: u8, // enumify this
+    // name_size: u16,
+    // attrs: u32,  // file attributes
+    // high_pack_size: u32,  // optional, todo: combine with pack size
+    // high_unpack_size: u32, // optional, todo: combine with unpack size
+    // file_name: vec![], // requires reference to name_size
+    // salt: [u8; 8], // optional, crypto salt
+    // ext_time: vec![] // optional, ext_time itself is variadic.
+}
+
+pub fn file_header(input: &[u8]) -> IResult<&[u8], FileHeader> {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn preamble_parse_matches_required_tag() {
+        let buf = RAR3_MAGIC;
+        let (rest, _) = block_preamble(0x73)(&buf);
+        assert_eq!(rest, &[]);
+    }
 }
